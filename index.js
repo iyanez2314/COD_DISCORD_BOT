cced8ffc878750826a381282e2feb5eecf4b8bd1
Isaac Yanez <isaacyanez@Isaacs-MBP-2.lan>
2022-06-16 19:02:57 -0500

    added welcome message to a user joining the server

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -13,8 +14,14 @@ client.on('ready', () => {
 
 client.on('messageCreate', (message) => {
     if(message.content === 'hi'){
-        message.reply('hello homo')
+        message.reply('hello')
     }
+})
+
+const welcomeChannelId = '987139870545817671'
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
 })
 
 client.login(process.env.TOKEN);
