43a63980b23be6fd473c4bd9831ad350ee5b5944
Isaac Yanez <isaacyanez@Isaacs-MBP-2.lan>
2022-07-05 22:17:44 -0500

    added some more fields to the lifetime stats and created some variables to hold the data so it makes the embed look a bit cleaner

--- commands/lifetime.js
+++ commands/lifetime.js
@@ -14,19 +14,25 @@ module.exports = {
         try {
             API.login(process.env.WZ_TOKEN);
             let data = await API.Warzone.fullData(args[0],API.platforms.Activision)
+            let lifetimeWins = data.data.lifetime.mode.br.properties.wins.toString();
+            let lifetimeKd = data.data.lifetime.mode.br.properties.kdRatio.toFixed(2).toString();
+            let lifetimeKills = data.data.lifetime.mode.br.properties.kills.toString();
+            let lifetimeGamesPlayed = data.data.lifetime.mode.br.properties.gamesPlayed.toString();
+            let lifetimeTopTen = data.data.lifetime.mode.br.properties.topTen.toString();
+            let lifetimeTopFive = data.data.lifetime.mode.br.properties.topFive.toString();
+            let lifetimeTopTwentyFive = data.data.lifetime.mode.br.properties.topTwentyFive.toString();
 
             const embed = new Discord.MessageEmbed()
             .setColor('#BFCDEB')
-            .setTitle('Life time stats')
-            .addFields(
-                {name: 'Wins', value: data.data.lifetime.mode.br.properties.wins.toString()},
-                {name: 'KD', value: data.data.lifetime.mode.br.properties.kdRatio.toFixed(2).toString()},
-                {name: 'Kills', value: data.data.lifetime.mode.br.properties.kills.toString()},
-                {name: 'Games Played', value: data.data.lifetime.mode.br.properties.gamesPlayed.toString()}
-
-            )
-            .setFooter({ text: 'Created by cozy', iconURL: 'https://i.imgur.com/AfFp7pu.png'})
-            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+            .setTitle('Lifetime Stats')
+            .addField('Lifetime Wins', `${lifetimeWins}`, true)
+            .addField('Lifetime KD', `${lifetimeKd}`, true)
+            .addField('Lifetime Kills', `${lifetimeKills}`, true)
+            .addField('Lifetime Games Played', `${lifetimeGamesPlayed}`, true)
+            .addField('Lifetime Top 5', `${lifetimeTopFive}`, true)
+            .addField('Lifetime Top 10', `${lifetimeTopTen}`, true)
+            .addField('Lifetime Top 25', `${lifetimeTopTwentyFive}`, true)
+            .setFooter({ text: 'Created by cozy'})
             .setTimestamp()
             message.channel.send({ embeds: [embed] }); 
         } catch(error){
